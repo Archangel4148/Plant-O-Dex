899e21eeda710c3978797e70d029175f7c3bbd31
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-11-02 17:07:58 -0500

    local storage for "found" plants!

--- components/FindPlant.js
+++ components/FindPlant.js
@@ -1,31 +1,31 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const storeUser = async (user) => {
-    try {
-      const jsonValue = JSON.stringify(userObject);
-      await AsyncStorage.setItem('userObject', jsonValue);
-    } catch (e) {
-      // saving error
-    }
-  };
-
-  const getUser = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem('userObject');
-      return jsonValue != null ? JSON.parse(jsonValue) : {foundPlants: []};
-    } catch (e) {
-      // error reading value
-    }
-  };
 
 export default FindPlant = async (plant) => {
+    const getUser = async () => {
+        try {
+          const jsonValue = await AsyncStorage.getItem('userObject');
+          return (jsonValue != null) ? JSON.parse(jsonValue) : {foundPlants: [],};
+        } catch (e) {
+          // error reading value
+        }
+      };
+      const storeUser = async (user) => {
+        try {
+          const jsonValue = JSON.stringify(user);
+          await AsyncStorage.setItem('userObject', jsonValue);
+        } catch (e) {
+          // saving error
+        }
+      };
+
+
     try {
         const userObject = await getUser()
-        userObject.then(() => {
-            console.log(userObject[userObject])
-        })
-        
-        userObject[foundPlants].push(plant)
+        console.log(userObject)
+        if (!userObject["foundPlants"].includes(plant.toLowerCase())) {
+            userObject["foundPlants"].push(plant.toLowerCase())
+        }
         await storeUser(userObject)
     }
     catch (e) {
