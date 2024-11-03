f6d4dc8b5582276e7d4e2f4c1b347161493267bd
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-11-03 08:20:43 -0600

    first

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,64 +1,89 @@
-import { TouchableOpacity, Text, View, ScrollView, TextInput, Pressable, Image, Dimensions } from "react-native";
+import { Text, View, ScrollView, TextInput, Pressable, Image } from "react-native";
 import { StyleSheet } from "react-native";
-import React, { useState } from 'react'; 
-import { useFonts } from "expo-font";
+import React, { useEffect, useState } from 'react'; 
+import {ImageView} from '@/components/ImageView.js'
+
+import {plantData} from "@/assets/plant_data/json_data/0_combined_plants.js"
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {router} from 'expo-router'
 
-import {ImageView} from '@/components/ImageView.js'
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {plantData} from "@/assets/plant_data/json_data/0_combined_plants.js"
 
-const {screenWidth, screenHeight} = Dimensions.get('window');
-const populatePlants = (searchTerm) => {
-  let plantsArr = [];
-  for (let plant in plantData) {
-    const commonName = plant.replace(/_/g, " ");
 
-    // Filter plants based on the search term
-    if (commonName.toLowerCase().includes(searchTerm.toLowerCase())) {
-      let src = ImageView[commonName.toLowerCase()]
 
-      const newPlant = (
-        <TouchableOpacity 
-          key={commonName} 
-          onPress={() => { router.push({
-            pathname: "/screens/plant_locked", params: {plant: commonName}
-          }); 
-        }}          
-        style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}>
-          <View style={styles.plantBox}>
-            <Image 
-              height={100} 
-              width={100} 
-              style={{ height: '100%', width: '100%', borderRadius: 10,}} 
-              source={src} 
-            />
-          </View>
-          <View style={{display: 'flex', justifyContent: 'center', alignContent: 'center',backgroundColor: 'rgba(0,0,0,0.5)', width: 160, height: 40, transform: "translateY(-10px)"}}>
-            <Text style={{textTransform: 'capitalize', textAlign: 'center', color: 'rgba(255,255,255,0.9)'}}>{commonName}</Text>
-          </View>
-        </TouchableOpacity>
-      );
-
-      if (src) {
-        plantsArr.push(newPlant);
-      }
-    }
-  }
-
-  return plantsArr;
-};
 
 export default function home() {
   const [searchTerm, setSearchTerm] = useState(''); // Initializing state
-  const [loaded, error] = useFonts({
-    'JetBrains': require('@/assets/fonts/JetBrainsMono-Medium.ttf'),
-  });
+  const [userArray, setUserArray] = useState({foundPlants: [],})
+
+  const getUser = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('userObject');
+      setUserArray((jsonValue != null) ? JSON.parse(jsonValue) : {foundPlants: [],});
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  const populatePlants = (searchTerm) => {
+    let plantsArr = [];
+    for (let plant in plantData) {
+      const commonName = plant.replace(/_/g, " ");
+  
+      // Filter plants based on the search term
+      if (commonName.toLowerCase().includes(searchTerm.toLowerCase())) {
+        // let src = require(`@/assets/plant_data/images/` + 
+        //   commonName.toLowerCase().replace(" ", "_") + '.png');
+        let src = ImageView[commonName.toLowerCase()]
+        let gold = require ('@/assets/temp_images/gold_border.png')
+  
+        const newPlant = (
+          <Pressable 
+            key={commonName} 
+            onPress={() => { router.push({
+              pathname: "/screens/plant_locked", params: {plant: commonName}
+            }); 
+          }}          
+          style={({ pressed }) => ([{ opacity: pressed ? 0.5 : 1 },  {position: 'relative'}])}>
+            <View style={[styles.plantBox, {position:'relative'}]}>
+              <Image 
+                height={100} 
+                width={100} 
+                style={{ height: '100%', width: '100%', borderRadius: 10,}} 
+                source={src} 
+              />
+            </View>
+            <View style={{display: 'flex', justifyContent: 'center', alignContent: 'center',backgroundColor: 'rgba(0,0,0,0.3)', width: 190, height: 40, transform: "translateY(-10px)"}}>
+              <Text style={{textTransform: 'capitalize', textAlign: 'center', color: 'rgba(255,255,255,0.8)'}}>{commonName}</Text>
+            </View>
+            {(userArray["foundPlants"] && userArray["foundPlants"].includes(commonName) &&
+            <Image source={gold} style={{position: 'absolute', width: 190, height: 190}}></Image>
+            )}
+            {() => {console.log(commonName)}}
+          </Pressable>
+        );
+  
+        if (src) {
+          plantsArr.push(newPlant);
+        }
+      }
+    }
+  
+    return plantsArr;
+  };
+  
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  useEffect(() => {
+    console.log(userArray)
+    console.log()
+  }, [userArray])
   return (
   <View style={{flex: 1}}>
 
     <View style={[styles.headerContainer, {backgroundColor: "#E3FFE5"}]}>
-      <Text style={[styles.headerText, {fontFamily: 'JetBrains'}]}>All Plants</Text>
+      <Text style={[styles.headerText, {fontFamily: 'JetBrains'}, {fontSize: 35}]}>All Plants</Text>
     </View>
     <ScrollView style={{backgroundColor: "#E3FFE5"}}>
       <View style={[styles.webContainer]}>
@@ -96,8 +121,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     marginTop: 10,
-    backgroundColor: "#E3FFE5", //The middle of the backround
-    width: screenWidth,
+    backgroundColor: "#E3FFE5" //The middle of the backround
   },
   headerContainer: {
     backgroundColor: '#d2d3db',
@@ -118,7 +142,7 @@ const styles = StyleSheet.create({
     
   },
   searchContainer: {
-    width: '95%',
+    width: '100%',
 
     display: 'flex',
     flexDirection: 'row',
@@ -137,7 +161,7 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     alignItems: 'center',
     
-    width: '100%',
+    width: 360,
     height: 45,
     
     paddingLeft: 10,
@@ -149,7 +173,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     color: 'white',
     fontSize: 20,
-    width: '100%'
+    width: 340
   },
 
   sortButton: {
@@ -169,26 +193,28 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     gap: 5,
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+    
     flexWrap: 'wrap',
     backgroundColor: "#E3FFE5", //Color for the backround bottom of the app
 
-    width: '95%',
+    width: 400,
     paddingBottom: 200,
     minHeight: 1000,
+
     marginTop: 15,
   },
 
   plantBox: {
     borderWidth: 1,
-    width: 160, 
-    height: 160,
+    width: 190, 
+    height: 190,
     borderColor: "#BDFFC2",
 
     backgroundColor: '#555555',
     borderRadius: 10,
     marginBottom: -40,
-    marginTop: 10,
+
   }
 
 })
