ff80464ea7fde432c77c4d433ea1f217767a0340
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-11-02 22:54:12 -0500

    some camera stuff :()

--- app/screens/camera.js
+++ app/screens/camera.js
@@ -1,11 +1,48 @@
 import { CameraView, Camera,  CameraType, useCameraPermissions, takePictureAsync} from 'expo-camera';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useFocusEffect } from 'expo-router';
+import { useCallback } from 'react';
+import * as FileSystem from 'expo-file-system';
+
+
 
 export default function camera() {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
   const cameraRef = useRef(null);
+
+  const saveImage = async (uri) => {
+    try {
+      // Define the file path where the image will be saved
+      const fileUri = FileSystem.documentDirectory + '/test.jpg';
+      
+      // Download the image from the URI
+      const response = await FileSystem.downloadAsync(uri, fileUri);
+      console.log(response)
+      // Check if the download was successful
+      
+    } catch (error) {
+      console.error(error);
+        }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log('Hello, I am focused!');
+      if (cameraRef.current) {
+        cameraRef.current.resumePreview()
+      }
+      return () => {
+        console.log('This route is now unfocused.');
+        if (cameraRef.current) {
+            cameraRef.current.pausePreview()
+          }
+        
+      }
+    }, [])
+  );
+  
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -24,8 +61,10 @@ export default function camera() {
 
   const captureImage = async () => {
     if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();
-      console.log(photo); // You can handle the photo here (e.g., display it, save it, etc.)
+        const options = { quality: 0.5, base64: true };
+      const photo = await cameraRef.current.takePictureAsync(options);
+      saveImage(photo.uri)
+      console.log(photo.uri); // You can handle the photo here (e.g., display it, save it, etc.)
     }
   };
 
@@ -33,15 +72,14 @@ export default function camera() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
-  
   return (
     <View style={styles.container}>
         
-      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+      <CameraView  style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
             <View style={{display: 'flex', alignSelf: 'flex-end', alignItems: 'center', flexDirection: 'row', alignContent: 'center'}}>
-                <TouchableOpacity style={{ width: 90, height: 90, borderRadius: 100, borderWidth: 6, borderColor: 'white'}}>
-                
+                <TouchableOpacity onPress={captureImage} style={{ width: 90, height: 90, borderRadius: 100, borderWidth: 6, borderColor: 'white'}}>
+
                 </TouchableOpacity>
           </View>
         </View>
