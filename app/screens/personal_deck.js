0f19b768344fbdd30c8ac3c2bd9d49e5d4355b49
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-11-02 21:17:06 -0500

    new butttonssss

--- app/screens/personal_deck.js
+++ app/screens/personal_deck.js
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, TextInput, Pressable, Image } from "react-native";
+import { TouchableOpacity, Text, View, ScrollView, TextInput, Pressable, Image } from "react-native";
 import { StyleSheet } from "react-native";
 import React, { useState, useEffect } from 'react'; 
 import {ImageView} from '@/components/ImageView.js'
@@ -38,7 +38,7 @@ export default function home() {
         let src = ImageView[commonName.toLowerCase()]
   
         const newPlant = (
-          <Pressable 
+          <TouchableOpacity 
             key={commonName} 
             onPress={() => { router.push({
               pathname: "/screens/plant_locked", params: {plant: commonName}
@@ -56,7 +56,7 @@ export default function home() {
             <View style={{display: 'flex', justifyContent: 'center', alignContent: 'center',backgroundColor: 'rgba(0,0,0,0.3)', width: 190, height: 40, transform: "translateY(-10px)"}}>
               <Text style={{textTransform: 'capitalize', textAlign: 'center', color: 'rgba(255,255,255,0.8)'}}>{commonName}</Text>
             </View>
-          </Pressable>
+          </TouchableOpacity>
         );
   
         if (src && user["foundPlants"].includes(commonName.toLowerCase())) {
